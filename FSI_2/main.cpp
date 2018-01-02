9721662399758e6c511251fb9d0d581c9d77b050
Aidan Hamilton <aidan.hamilton47@gmail.com>
2018-01-01 16:24:02 -0800

    Initial Commit

---
+++ FSI_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FSI_2
+//
+//  Created by Aidan Hamilton on 1/1/18.
+//  Copyright © 2018 Aidan Hamilton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
